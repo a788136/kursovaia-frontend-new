38ce8f0b8254993c373888e962de5ec70c8549b9
Artem Mamian <a788136@gmail.com>
2025-08-19 14:22:15 +0400

    create from email user

--- src/api/http.js
+++ src/api/http.js
@@ -1,15 +1,23 @@
-// front/src/api/http.js
+// src/api/http.js
 import axios from 'axios';
 import { getToken } from './token';
 
+const API_BASE = (import.meta.env.VITE_API_URL || '').replace(/\/+$/, '');
+
 const http = axios.create({
-  baseURL: '/api',
-  withCredentials: true, // обязательно, чтобы сессия доходила
+  baseURL: API_BASE || undefined,
+  withCredentials: true,
 });
 
 http.interceptors.request.use((config) => {
   const t = getToken?.();
-  if (t) config.headers.Authorization = `Bearer ${t}`;
+  // Не подставляем сомнительные/пустые маркеры
+  if (t && t !== 'cookie-session') {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${t}`;
+  } else if (config.headers?.Authorization) {
+    delete config.headers.Authorization;
+  }
   return config;
 });
 
